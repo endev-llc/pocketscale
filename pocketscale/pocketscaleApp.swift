bfc51e78a6a35ea15dc5f6909345cba6a9f6c5fd
jakeadams3 <jakeadams1230@gmail.com>
2025-06-30 15:11:53 -0400

    Initial Commit

---
+++ pocketscale/pocketscaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  pocketscaleApp.swift
+//  pocketscale
+//
+//  Created by Jake Adams on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct pocketscaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pocketscaleTests/pocketscaleTests.swift
@@ -0,0 +1,17 @@
+//
+//  pocketscaleTests.swift
+//  pocketscaleTests
+//
+//  Created by Jake Adams on 6/30/25.
+//
+
+import Testing
+@testable import pocketscale
+
+struct pocketscaleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
