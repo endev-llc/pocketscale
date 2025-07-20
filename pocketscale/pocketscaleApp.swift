78603790f573108fa0f59138728c66ace67a46d9
jakeadams3 <jakeadams1230@gmail.com>
2025-07-20 12:38:19 -0400

    reverted to previous version of app by removing acces code screen.

--- pocketscale/pocketscaleApp.swift
+++ pocketscale/pocketscaleApp.swift
@@ -14,10 +14,6 @@ struct pocketscaleApp: App {
     @StateObject private var authStateObserver = AuthStateObserver()
     @StateObject private var subscriptionManager = SubscriptionManager.shared
     
-    // @AppStorage automatically reads from and writes to UserDefaults.
-    // The app will now remember if the access code has been entered.
-    @AppStorage("hasEnteredEarlyAccessCode") private var hasEnteredEarlyAccessCode: Bool = false
-
     init() {
         FirebaseApp.configure()
     }
@@ -25,31 +21,22 @@ struct pocketscaleApp: App {
     var body: some Scene {
         WindowGroup {
             Group {
-                // First, check if the user has already been granted early access.
-                if !hasEnteredEarlyAccessCode {
-                    // If not, show the AccessView. It will handle its own logic
-                    // and update the "hasEnteredEarlyAccessCode" value in UserDefaults.
-                    AccessView()
-                } else if authStateObserver.isLoading {
+                if authStateObserver.isLoading {
                     // Show nothing while Firebase determines auth state (prevents flash)
                     Color(.systemBackground)
                         .ignoresSafeArea()
                 } else if authStateObserver.user != nil {
-                    // User is signed in
                     if subscriptionManager.hasAccessToApp {
                         MainView()
                     } else {
                         SubscriptionView()
                     }
                 } else {
-                    // User is not signed in, but has passed the access code screen
                     MainView()
                 }
             }
             .environmentObject(authStateObserver)
             .environmentObject(subscriptionManager)
-            // Animate the transition from AccessView to the main app content.
-            .animation(.easeInOut(duration: 0.4), value: hasEnteredEarlyAccessCode)
             .animation(.easeInOut(duration: 0.3), value: authStateObserver.user != nil)
             .animation(.easeInOut(duration: 0.3), value: subscriptionManager.hasAccessToApp)
             .onChange(of: authStateObserver.user) { oldUser, newUser in
